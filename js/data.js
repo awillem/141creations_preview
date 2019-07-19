f97f0507bf96c4317c9e890995276f6eb6e33d65
Emma Willmann <emma.anne.81@gmail.com>
2019-07-18 20:07:00 -0500

    added carousel to modals

---
+++ js/data.js
@@ -0,0 +1,11 @@
+let products = [
+  {
+    id: 1,
+    type: 'pen',
+    material: 'acrylic',
+    kitType: 'clicker',
+    kitColor: 'pewter',
+    images: ['img/Pen Images/pen_1.jpeg', 'img/Pen Images/pen_2.jpeg', 'img/Pen Images/pen_3.jpeg'],
+    sold: false
+  }
+]
