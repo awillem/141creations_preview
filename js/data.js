ec7f3ea703aebecb5f28562c340c6af2ebb2c648
Emma Willmann <emma.anne.81@gmail.com>
2019-07-18 22:27:04 -0500

    set up product data to show dynamically based on page, added pagination

--- js/data.js
+++ js/data.js
@@ -3,9 +3,63 @@ let products = [
     id: 1,
     type: 'pen',
     material: 'acrylic',
-    kitType: 'clicker',
+    kitType: 'grenade',
     kitColor: 'pewter',
     images: ['img/Pen Images/pen_1.jpeg', 'img/Pen Images/pen_2.jpeg', 'img/Pen Images/pen_3.jpeg'],
+    sold: false
+  },
+  {
+    id: 2,
+    type: 'bottle opener',
+    material: 'acrylic',
+    kitType: 'clicker',
+    kitColor: 'silver',
+    images: ['img/Pen Images/pen_2.jpeg', 'img/Pen Images/pen_3.jpeg', 'img/Pen Images/pen_4.jpeg'],
+    sold: false
+  },
+  {
+    id: 3,
+    type: 'pen',
+    material: 'acrylic',
+    kitType: 'twist skull',
+    kitColor: 'pewter',
+    images: ['img/Pen Images/pen_3.jpeg', 'img/Pen Images/pen_4.jpeg', 'img/Pen Images/pen_5.jpeg'],
+    sold: false
+  },
+  {
+    id: 4,
+    type: 'pen',
+    material: 'acrylic',
+    kitType: 'twist',
+    kitColor: 'gunmetal',
+    images: ['img/Pen Images/pen_4.jpeg', 'img/Pen Images/pen_5.jpeg', 'img/Pen Images/pen_6.jpeg'],
+    sold: false
+  },
+  {
+    id: 5,
+    type: 'pen',
+    material: 'acrylic',
+    kitType: 'clicker',
+    kitColor: 'silver',
+    images: ['img/Pen Images/pen_5.jpeg', 'img/Pen Images/pen_6.jpeg', 'img/Pen Images/pen_7.jpeg'],
+    sold: false
+  },
+  {
+    id: 6,
+    type: 'pen',
+    material: 'wood',
+    kitType: 'cap',
+    kitColor: 'gunmetal',
+    images: ['img/Pen Images/pen_6.jpeg', 'img/Pen Images/pen_7.jpeg', 'img/Pen Images/pen_1.jpeg'],
+    sold: false
+  },
+  {
+    id: 7,
+    type: 'pen',
+    material: 'acrylic',
+    kitType: 'spring cap',
+    kitColor: 'silver',
+    images: ['img/Pen Images/pen_7.jpeg', 'img/Pen Images/pen_1.jpeg', 'img/Pen Images/pen_2.jpeg'],
     sold: false
   }
 ]
--- js/script.js
+++ js/script.js
@@ -1,11 +1,139 @@
+
+const paginationUL = document.querySelector('ul.pagination');
+const cardContainer = document.querySelector('main div.container');
+let displayQty = 4;
 let currentList = products;
 console.log(currentList);
-// const cardTitles = Array.from(document.querySelectorAll('.card-title span'));
-// cardTitles.forEach((title, i) => {
-//   if (title.innerText.length > 14) {
-//     let newTitle = `${title.innerText.slice(0, 12)}...`;
-//     cardTitles[i].innerText = newTitle;
-//   }
-// });
+
+function showPage(list, page, displayQty) {
+  let start;
+  let end;
+  if (page === 0) {
+    start = 0;
+    end = list.length - 1;
+  } else {
+    start = ((page * displayQty) - 1) - (displayQty - 1);
+    end = (page * displayQty) - 1;
+    if (end >= list.length) {
+      end = list.length - 1;
+    }
+  }
+
+  console.log(start, end);
+
+  let html = ``;
+  let row = `<div class="row">`;
+  for (let i = start; i <= end; i++) {
+    row += productHtml(list[i], i);
+
+    // console.log(i === 0, (i + 1) % 4 === 0, (i + 1) === list.length)
+
+    if ((i + 1) === list.length) {
+      row += `</div>`;
+      html += row;
+    } else if (i === 0) {
+      // do nothing
+    } else if ((i + 1) % 4 === 0) {
+      row += `</div>`;
+      html += row;
+      row = `<div class="row">`;
+    }
+  }
+  // console.log(html)
+  cardContainer.innerHTML = html;
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {
+    'onOpenEnd': initCarouselModal
+  });
+
+  function initCarouselModal() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+    instances[0].set(2);
+  }
+}
 
 
+
+/*
+  Removes previous page links, if list.length is less than the displayQty,
+  it will not show any links.  Otherwise, it will show links based on the list length
+*/
+function pageLinks(list, displayQty) {
+  paginationUL.innerHTML = '';
+  if (list.length <= displayQty) {
+    //showPage(list, 0);
+  } else {
+    let pages = Math.ceil(list.length / displayQty);
+    for (let i = 1; i <= pages; i++) {
+      let li = document.createElement('li');
+      if (i === 1) {
+        li.classList.add('active');
+      }
+      let a = document.createElement('a');
+      a.innerText = i;
+      li.append(a);
+      paginationUL.append(li);
+    }
+  }
+  showPage(list, 1, displayQty);
+}
+
+pageLinks(currentList, displayQty);
+
+
+paginationUL.addEventListener('click', e => {
+  console.log(e.target.innerText);
+  if (e.target.tagName === 'A') {
+    let links = document.querySelectorAll('.pagination li');
+    console.log(links)
+    links.forEach(link => {
+      link.classList.remove('active');
+    });
+    e.target.parentNode.classList.add('active');
+    showPage(currentList, e.target.innerText, displayQty)
+  }
+})
+
+
+
+
+/*
+  Creates the HTML to display a product as a card, with the "reveal" information, and the image carousel in the modal
+  Takes in a product object
+*/
+function productHtml(product, index) {
+  // console.log(product);
+  let html = `
+  <div class="col s12 m4 l3">
+  <div class="card hoverable">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="${product.images[0]}">
+    </div>
+    <div class="card-content">
+      <span class="activator grey-text text-darken-"><span><a class="modal-trigger" href="#modal${index}">More
+            Images</a><i class="material-icons right">info_outline</i></span>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+
+      <ul class="collection">
+        <li class="collection-item"><span>Type: </span><span>${product.type}</span></li>
+        <li class="collection-item"><span>Material: </span><span>${product.material}</span></li>
+        <li class="collection-item"><span>Kit Type: </span><span>${product.kitType}</span></li>
+        <li class="collection-item"><span>Kit Metal Color: </span><span>${product.kitColor}</span></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div id="modal${index}" class="modal">
+            <div class="carousel">
+              <a class="carousel-item" href="#one!"><img src="${product.images[0]}"></a>
+              <a class="carousel-item" href="#two!"><img src="${product.images[1]}"></a>
+              <a class="carousel-item" href="#three!"><img src="${product.images[2]}"></a>
+            </div>
+          </div>
+  `;
+  return html;
+}
